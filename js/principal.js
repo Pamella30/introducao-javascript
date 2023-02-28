a9033c184227f27643f467e711aa23d2cbbb21ad
Pamella Cruz <suporte.pamella@gmail.com>
2023-02-28 13:48:29 -0300

    update

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Brasil Nutrição";
 
 var paciente = document.querySelector("#primeiro-paciente");
 
@@ -10,5 +10,23 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura)
-tdImc.textContent = imc;
+
+pesoValido = true;
+alturaValido = true;
+
+if (peso <= 0 || peso >= 500){
+    console.log("Peso invalido!")
+    pesoValido = false;
+    tdImc.textContent = "Peso invalido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura invalida!")
+    alturaValido = false;
+    tdImc.textContent = "Altura invalida!"
+}
+
+if(pesoValido && alturaValido){
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc;
+}
