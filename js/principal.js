30d2eb5c006930d0e5760ca5028637894ff9368f
Pamella Cruz <suporte.pamella@gmail.com>
2023-03-01 08:00:45 -0300

    update

--- js/principal.js
+++ js/principal.js
@@ -1,32 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Brasil Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i]
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-pesoValido = true;
-alturaValido = true;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 500){
-    console.log("Peso invalido!")
-    pesoValido = false;
-    tdImc.textContent = "Peso invalido!";
+    var tdImc = paciente.querySelector(".info-imc");
+
+    pesoValido = true;
+    alturaValido = true;
+
+    if (peso <= 0 || peso >= 500){
+        console.log("Peso invalido!")
+        pesoValido = false;
+        tdImc.textContent = "Peso invalido!";
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura invalida!")
+        alturaValido = false;
+        tdImc.textContent = "Altura invalida!"
+    }
+
+    if(pesoValido && alturaValido){
+        var imc = peso / (altura * altura)
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 }
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura invalida!")
-    alturaValido = false;
-    tdImc.textContent = "Altura invalida!"
-}
-
-if(pesoValido && alturaValido){
-    var imc = peso / (altura * altura)
-    tdImc.textContent = imc;
-}
