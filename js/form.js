f418b0ea56162895e13c47e3be75146d1a3f7eb9
Pamella Cruz <suporte.pamella@gmail.com>
2023-03-07 13:51:31 -0300

    update

--- js/form.js
+++ js/form.js
@@ -4,32 +4,17 @@ botaoAdicionar.addEventListener("click", function(event) {
     console.log("Oi eu sou o botao e fui clicado");
 
 
-var form = document.querySelector("#form-adiciona");
-var paciente = obtemFormDoPaciente(form);
+var form = document.querySelector("#form-adiciona");  //pegando o form
 
-var pacienteTr = document.createElement("tr");
+var paciente = obtemFormDoPaciente(form); // pega o paciente do form
 
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
+var pacienteTr = montaTr(paciente); // monta uma tr a partir do paciente
 
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calcImc(peso, altura);
-
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(imcTd);
-
-var tabela = document.querySelector("#tabela-pacientes");
+var tabela = document.querySelector("#tabela-pacientes"); // adiciona o paciente na tabela
 
 tabela.appendChild(pacienteTr);
+
+form.reset(); // limpa o campo
 
 });
 
@@ -46,3 +31,25 @@ function obtemFormDoPaciente(form){
     return paciente;
 
 }
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+
+    }
+
+    function montaTd(dado, classe){
+        var td = document.createElement("td");
+        td.textContent = dado;
+        td.classList.add(classe);
+    
+        return td;
+    }
